ecec33154378bea9c544dd71aa0a350eb9bf8b8d
Wesley Almeida <wesley.almeida@fotosensores.com>
2024-01-12 12:46:26 -0300

    viewsets added

--- school/urls.py
+++ school/urls.py
@@ -1,2 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import StudentViewSet, CourseViewSet
+
+router = routers.DefaultRouter()
+router.register('students', StudentViewSet, basename='Students')
+router.register('courses', CourseViewSet, basename='Courses')
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
--- school/views.py
+++ school/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Student, Course
+from .serializers import StudentSerializer, CourseSerializer
 
-# Create your views here.
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
